f4c25503e77242e2851aeb939df0136115e7a1bb
PatrickkikALT <45902985+PatrickkikALT@users.noreply.github.com>
2025-02-10 16:45:38 +0100

    Player Movement + start at camera

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Vector2 _input;
+    private Rigidbody _rb;
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        _input = ctx.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) return;
+
+        if (OnGround)
+        {
+            jumping = true;
+            _rb.velocity = new Vector3(0, jumpStrength, 0);
+        }
+    }
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 dir = transform.TransformDirection(new Vector3(_input.x, 0, _input.y));
+        _rb.velocity = new Vector3(dir.x * speed, _rb.velocity.y, dir.z * speed);
+        if (jumping)
+        {
+            UpdateJumpMomentum();
+        }
+    }
+    
+    [SerializeField] private bool jumping;
+
+    [SerializeField] private float fallVel;
+    [SerializeField] private float jumpStrength;
+
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private float overlapSphereSize;
+
+    public bool OnGround => Physics.OverlapSphere(groundCheckTransform.position, overlapSphereSize, groundLayers).Length > 0;
+
+
+    private void UpdateJumpMomentum()
+    {
+        if (_rb.velocity.y < 0.5)
+        {
+            _rb.velocity -= new Vector3(0, fallVel * Time.deltaTime, 0);
+
+            if (OnGround)
+            {
+                jumping = false;
+            }
+        }
+    }
+}
